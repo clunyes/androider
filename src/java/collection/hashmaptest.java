c13f6f1cbdf1a97b5c9ae6f250f84c773f13df96
赵康 <kang@wlwbshop.com>
2018-09-11 17:04:25 +0800

    collection

--- src/java/collection/hashmaptest.java
+++ src/java/collection/hashmaptest.java
@@ -1,4 +1,17 @@
 package java.collection;
 
+import java.util.HashMap;
+
 public class hashmaptest {
+    /**
+     * https://www.cnblogs.com/chengxiao/p/6059914.html
+     *
+     * 一个数组，数组里存的链表
+     *
+     * 根据代码i去数组上找，如果有相同entry，替换，没有，加入链表
+     *
+     */
+
+//    int hash = hash(key.hashCode());//对key的hashcode进一步计算，确保散列均匀
+//    int i = indexFor(hash, table.length);//获取在table中的实际位置
 }
