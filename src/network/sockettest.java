cb0cdefba036f525b6e40794175b36a8e5d4dbed
赵康 <kang@wlwbshop.com>
2018-09-20 16:51:02 +0800

    email info

---
+++ src/network/sockettest.java
@@ -0,0 +1,15 @@
+package network;
+
+public class sockettest {
+    /**
+     * socket则是对TCP/IP协议的封装和应用(程序员层面上)。
+     * 也可以说，TPC/IP协议是传输层协议，主要解决数据如何在网络中传输，
+     *　而HTTP是应用层协议，主要解决如何包装数据。
+     *
+     *
+     * Socket的出现只是使得程序员更方便地使用TCP/IP协议栈而已，是对TCP/IP协议的抽象，
+     * 从而形成了我们知道的一些最基本的函数接口，比如create、listen、connect、accept、send、read和write等等。
+     *
+     * 比喻：HTTP是轿车，提供了封装或者显示数据的具体形式;Socket是发动机，提供了网络通信的能力。
+     */
+}
