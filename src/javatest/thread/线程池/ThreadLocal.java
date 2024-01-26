2741e6750a54ce066b03ee1844093551cb7b9f4e
kang.zhao <kang.zhao@ecarx.com.cn>
2024-01-26 17:38:37 +0800

    feat:线程

--- src/javatest/thread/线程池/ThreadLocal.java
+++ src/javatest/thread/线程池/ThreadLocal.java
@@ -6,6 +6,10 @@ public class ThreadLocal {
      * 各个线程之间的变量互不干扰，在高并发场景下，可以实现无状态的调用，特别适用于各个线程依赖不同的变量值完成操作的场景。
      *
      *
+     * android handler使用了这个类
+     *
+     * Looper 在构造时同步会创建 MessageQueue，并利用 ThreadLocal 这种 TLS，将其与当前线程绑定。（TLS ： Thread Local Storage）
+     *
      * 其中ThreadLocalMap类的定义是在ThreadLocal类中，真正的引用却是在Thread类中。
      * Map的key是ThreadLocal类的实例对象，value为用户的值
      */
